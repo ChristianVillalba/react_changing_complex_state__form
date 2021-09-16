f4e1b7ab31eb1a54eea1a7f9d174d2133249be8d
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-09-16 15:00:56 +0100

    First Challenge Completed: Update the greeting heding (H1) unsing the inputs provided by the User "I want to be able to greet the user using both of these inputs. The inputs are stateful, independent and they can be updated. Changes will be reflected in the Heading"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFirstName] = useState("");
+  const [lName, setLastName] = useState("");
+
+  function handlefNameChange() {
+    setFirstName(event.target.value);
+  }
+  function handlelNameChange() {
+    setLastName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          value={fName}
+          type="text"
+          onChange={handlefNameChange}
+          placeholder="First Name"
+        />
+        <input
+          value={lName}
+          type="text"
+          onChange={handlelNameChange}
+          placeholder="Last Name"
+        />
         <button>Submit</button>
       </form>
     </div>
